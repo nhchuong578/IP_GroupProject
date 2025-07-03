ee08ff13eaa508c1d5550c05524fab1319be2638
KhangNguyen91206 <khangphuc912@gmail.com>
2025-07-03 15:21:48 +0700

    Update tags.h

--- include/tags.h
+++ include/tags.h
@@ -1,12 +1,6 @@
 #ifndef TAGS_H
 #define TAGS_H
-#define MAX_CATEGORY 30
-#define MAX_TAGS 20
-extern char predefinedTags[MAX_TAGS][MAX_CATEGORY];
-extern int tagCount;
-
 int findCategoryIndex(char categories[][MAX_CATEGORY], int catCount, char *target);
-
 void saveTags();
 void loadTags();
 void deleteTag();
